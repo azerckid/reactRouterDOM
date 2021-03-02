38bb67ddbc6db06f6aab9873e5a3cd510ee83372
zizimoos <zizimoos@banco.id>
2021-03-02 12:32:48 +0900

    clean3

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -9,7 +9,6 @@ import Header from "./Header";
 import Home from "Routes/Home/index";
 import Product from "Routes/Product";
 import Company from "Routes/Company";
-import Detail from "Routes/Detail";
 import RoadMap from "Routes/RoadMap";
 import TeamMember from "./others/TeamMember";
 
@@ -24,7 +23,6 @@ export default () => {
         <Route path="/company" component={Company}></Route>
         <Route path="/product" component={Product}></Route>
         <Route path="/team" component={TeamMember}></Route>
-        <Route path="/detail" component={Detail}></Route>
         <Route path="/roadmap" component={RoadMap}></Route>
         <Redirect from="*" to="/"></Redirect>
       </Switch>
--- src/Routes/Detail/index.js
+++
@@ -1,6 +0,0 @@
-import DetailContainer from "./DetailContainer";
-
-export default DetailContainer;
-//"predeploy": "npm run build",
-//"deploy": "gh-pages -d build"
-//"homepage": "https://zizimoos.github.io/cub_dice_hunter"
